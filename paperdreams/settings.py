be929b0f08a3df36518f3947dba55de5ede4f857
leah-jala <leahlafond@protonmail.com>
2023-03-23 16:43:26 +0000

    Added allauth templates and base.html

--- paperdreams/settings.py
+++ paperdreams/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+if os.path.isfile('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a*zzxch2ugvnwe2y_bz-!#2-zdc@0!5k8f@&2b&0z1rm2r+ps&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
