d57fbe2625a84d847929c4d11c8390edab074c87
leah-jala <leahlafond@protonmail.com>
2023-04-02 23:54:32 +0000

    Add and migrate wishlist model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"{self.user.username}'s Wishlist"
+
